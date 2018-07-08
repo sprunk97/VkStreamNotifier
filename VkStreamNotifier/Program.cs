26042782bfe18c53662c2cf2bc92caf8acce1bd0
Козлов Даниил <sprunk97@gmail.com>
2018-07-09 03:29:03 +0700

    Added menu

--- VkStreamNotifier/Program.cs
+++ VkStreamNotifier/Program.cs
@@ -4,9 +4,34 @@ namespace VkStreamNotifier
 {
     class Program
     {
+        private static Settings settings;
+
         static void Main(string[] args)
         {
-            Settings settings = SettingsReader.LoadSettings();
+            do
+            {
+                switch (Console.ReadLine())
+                {
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+                    case "load":
+                        Load();
+                        break;
+                    case "connect":
+                        Connect();
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized command. List of commands: load, connect");
+                        break;
+                }
+            }
+            while (true);
+        }
+
+        static void Load()
+        {
+            settings = SettingsReader.LoadSettings();
 
             Console.WriteLine("\tCurrent settings:");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -14,12 +39,12 @@ namespace VkStreamNotifier
                 Console.WriteLine($"{property.Name} : {property.GetValue(settings, null)}");
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine();
+        }
 
+        static void Connect()
+        {
             Twitch twitch = new Twitch(settings);
             twitch.CreateConnection();
-
-            if (Console.ReadLine() != "exit")
-                Console.ReadKey();
         }
     }
 }
