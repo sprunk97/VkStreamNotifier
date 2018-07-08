0fd1213d3946128616add0b70dc473d8bbf2a2a5
Козлов Даниил <sprunk97@gmail.com>
2018-07-09 04:19:59 +0700

    Singleton realisation

---
+++ VkStreamNotifier/Monitor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TwitchLib.Api;
+using TwitchLib.Api.Services;
+using TwitchLib.Api.Services.Events.LiveStreamMonitor;
+
+namespace VkStreamNotifier
+{
+    class Monitor
+    {
+        private LiveStreamMonitor monitor;
+        private Settings settings;
+        private static Monitor instance;
+
+        public Monitor() { }
+        protected Monitor(Settings settings, TwitchAPI api)
+        {
+            this.settings = settings;
+            Inititalize(api);
+        }
+
+        public static Monitor GetInstance(Settings settings, TwitchAPI api)
+        {
+            if (instance == null)
+                instance = new Monitor(settings, api);
+            else Console.WriteLine("Already connected");
+            return instance;
+        }
+
+        private void Inititalize(TwitchAPI api)
+        {
+            var userId = api.Users.v5.GetUserByNameAsync(settings.twitch_username).Result.Matches[0].Id;
+            monitor = new LiveStreamMonitor(api);
+            monitor.OnStreamOnline += new EventHandler<OnStreamOnlineArgs>(OnStreamOnline);
+            monitor.OnStreamOffline += new EventHandler<OnStreamOfflineArgs>(OnStreamOffline);
+            monitor.OnStreamMonitorStarted += new EventHandler<OnStreamMonitorStartedArgs>(OnMonitorStarted);
+            monitor.OnStreamMonitorEnded += new EventHandler<OnStreamMonitorEndedArgs>(OnMonitorEnded);
+
+            monitor.CheckIntervalSeconds = 60;
+            monitor.SetStreamsByUserId(new List<string>() { userId });
+            monitor.StartService();
+        }
+
+        private void OnStreamOffline(object sender, OnStreamOfflineArgs e)
+        {
+            Console.WriteLine($"{DateTime.Now} {settings.twitch_username} ended stream");
+        }
+
+        private void OnMonitorEnded(object sender, OnStreamMonitorEndedArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{DateTime.Now} Monitor ended");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private void OnMonitorStarted(object sender, OnStreamMonitorStartedArgs e)
+        {
+            Console.WriteLine($"{DateTime.Now} Monitor started");
+        }
+
+        private void OnStreamOnline(object sender, OnStreamOnlineArgs e)
+        {
+            Console.WriteLine($"{DateTime.Now} {settings.twitch_username} started stream");
+            VK vk = new VK(settings);
+            vk.Connect();
+        }
+    }
+}
--- VkStreamNotifier/Twitch.cs
+++ VkStreamNotifier/Twitch.cs
@@ -1,63 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-
-using TwitchLib.Api;
-using TwitchLib.Api.Services;
-using TwitchLib.Api.Services.Events.LiveStreamMonitor;
+﻿using TwitchLib.Api;
 
 namespace VkStreamNotifier
 {
     class Twitch
     {
-        private TwitchAPI api = new TwitchAPI();
-        private LiveStreamMonitor monitor;
         private Settings settings;
 
         public Twitch() { }
         public Twitch(Settings settings) => this.settings = settings;
 
         /// <summary>
-        /// Creates twitch api and monitor to handle events
+        /// Creates twitch api and monitor instance
         /// </summary>
         public void CreateConnection()
         {
+            TwitchAPI api = new TwitchAPI();
             api.Settings.ClientId = settings.twitch_id;
             api.Settings.AccessToken = settings.twitch_token;
-            var userId = api.Users.v5.GetUserByNameAsync(settings.twitch_username).Result.Matches[0].Id;
 
-            monitor = new LiveStreamMonitor(api);
-            monitor.OnStreamOnline += new EventHandler<OnStreamOnlineArgs>(OnStreamOnline);
-            monitor.OnStreamOffline += new EventHandler<OnStreamOfflineArgs>(OnStreamOffline);
-            monitor.OnStreamMonitorStarted += new EventHandler<OnStreamMonitorStartedArgs>(OnMonitorStarted);
-            monitor.OnStreamMonitorEnded += new EventHandler<OnStreamMonitorEndedArgs>(OnMonitorEnded);
-
-            monitor.CheckIntervalSeconds = 60;
-            monitor.SetStreamsByUserId(new List<string>() { userId });
-            monitor.StartService();
-        }
-
-        private void OnStreamOffline(object sender, OnStreamOfflineArgs e)
-        {
-            Console.WriteLine($"{DateTime.Now} {settings.twitch_username} ended stream");
-        }
-
-        private void OnMonitorEnded(object sender, OnStreamMonitorEndedArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{DateTime.Now} Monitor ended");
-            Console.ForegroundColor = ConsoleColor.Gray;
-        }
-
-        private void OnMonitorStarted(object sender, OnStreamMonitorStartedArgs e)
-        {
-            Console.WriteLine($"{DateTime.Now} Monitor started");
-        }
-
-        private void OnStreamOnline(object sender, OnStreamOnlineArgs e)
-        {
-            Console.WriteLine($"{DateTime.Now} {settings.twitch_username} started stream");
-            VK vk = new VK(settings);
-            vk.Connect();
+            var monitor = Monitor.GetInstance(settings, api);
         }
     }
 }
